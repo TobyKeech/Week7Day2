0262507a37fc7d43a9e8a6b4056f5aa45eba6c26
Toby Keech <toby.keech18@gmail.com>
2023-04-12 09:16:50 +0100

    chnaged some film titles

--- src/containers/MovieBox.js
+++ src/containers/MovieBox.js
@@ -12,22 +12,23 @@ const MovieBox = () => {
             },
             {
                 id: 2,
-                name: "Life Itself",
-                url: "https://www.imdb.com/title/tt5989218/?ref_=rlm"
+                name: "Hurt Locker",
+                url: "https://www.imdb.com/title/tt0887912/"
             },
             {
                 id: 3,
-                name: "Mary Queen of Scots",
-                url: "https://www.imdb.com/title/tt2328900/?ref_=rlm"
+                name: "Avengers: Endgame",
+                url: "https://www.imdb.com/title/tt4154796/"
             },
             {
                 id: 4,
-                name: "The Lego Movie 2: The Second Part", url: "https://www.imdb.com/title/tt3513498/?ref_=rlm"
+                name: "The Lego Movie 2: The Second Part", 
+                url: "https://www.imdb.com/title/tt3513498/?ref_=rlm"
             },
             {
                 id: 5,
-                name: "Captain Marvel",
-                url: "https://www.imdb.com/title/tt4154664/?ref_=rlm"
+                name: "John Wick",
+                url: "https://www.imdb.com/title/tt2911666/"
             }
         ]
     )
